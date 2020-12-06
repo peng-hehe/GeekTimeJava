b5b060d1eb8dbaf4afe5cbf5b7acf5abfa4b5d93
duyapeng <13643120414.163.com>
2020-12-06 16:03:08 +0800

    newclass

---
+++ supermaket/src/cn/com/supermarket1/LittleSuperMarket.java
@@ -0,0 +1,10 @@
+package cn.com.supermarket1;
+
+public class LittleSuperMarket {
+    public String superMarketName;
+    public String address;
+    public int parkingCount;
+    public double incomingSum;
+    public int[] merchandiseSole;
+
+}
